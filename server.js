9f128a57ddbdf27e4ff00fb27514109c26d4ce28
Hung Tran <hungtrn75@gmail.com>
2018-10-06 17:21:41 +0700

    deploy háºeroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
 
@@ -37,6 +38,17 @@ const posts = require("./routes/api/posts");
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//server static assets if it's production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client-refactor/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client-refactor", "build", "index.html")
+    );
+  });
+}
 
 const port = process.env.PORT || 5000;
 
